707f67683b7231906649c094d2d4f356b259489e
Petr <petiauzunov1997481@gmail.com>
2022-11-06 16:40:45 +0400

    add pullRequest

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println(" Задание №1 " );
+        int salary = 15000;
+        int total = 0;
+        for (int i = 0;total < 2459000; i++ ) {
+            total = total + total/100;
+            total = total + salary;
+          //  System.out.println(" Месяц " + i + " Итого " + total);
+        }
+        System.out.println(total);
+       // System.out.println(total);
+       // System.out.println(i);
+        //Задание 2
+        System.out.println( " Задание №2 ");
+        int a = 0;
+        while ( a < 10) {a++;
+        System.out.println(a);}
+        for (int b = 10; b > 0; b = b -1) {
+            System.out.println(b);}
+        //Задание 3
+        System.out.println(" Задание №3 ");
+         int people = 12_000_000;
+         int born = people / 1000 * 17;
+         int dead = people / 1000 * 8;
+       for ( int y = 0; y <= 10; y++ ) {people = people + born - dead ;
+        System.out.println(" За " + y + " год население выросло до " + people + " человек ");}
+
     }
 }
